1100813383786c58b556d37d7918b27f46fe82f7
Tanvirshahriargit <tanvirshahriar20188@gmail.com>
2021-09-02 04:42:27 +0600

    js and book search add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log('Js Found');
+const searchField = document.getElementById('search-field');
+const bookContainer = document.getElementById('book-container');
+// const searchBook = () => {
+//     search = searchField.value;
+//     console.log(search);
+//     // celar search box
+//     searchField.value = '';
+    
+// }
+// searchBook();
+const loadBook = () => {
+    search = searchField.value;
+    console.log(search);
+    // celar search box
+    searchField.value = '';
+    const url =`http://openlibrary.org/search.json?q=${search}` ;
+    // const url =(`http://openlibrary.org/search.json?q=javascript`) ;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayBook(data.docs))
+}
+loadBook();
+const displayBook = (books)=>{
+    books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('book-container');
+        div.classList.add('col-md-3');
+        div.innerHTML = `
+                <img class="img-fluid" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="">
+                <h2>${book.title}</h2>
+                <p>Book autor:${book.author_name}</p>
+                <p>${book.first_publish_year}</p>
+        `
+        bookContainer.appendChild(div);
+    });
+}
